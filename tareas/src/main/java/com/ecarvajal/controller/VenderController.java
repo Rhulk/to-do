b40d97928f6bb4ea502e6c304cdd19d2b63c9139
rhulk <quique1904@gmail.com>
2020-10-23 00:38:44 +0200

    xdocReport varios

--- tareas/src/main/java/com/ecarvajal/controller/VenderController.java
+++ tareas/src/main/java/com/ecarvajal/controller/VenderController.java
@@ -1,5 +1,10 @@
 package com.ecarvajal.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,6 +22,13 @@ import com.ecarvajal.model.ProductoParaVender;
 import com.ecarvajal.model.Tarea;
 import com.ecarvajal.service.Listas;
 import com.ecarvajal.xdoc.ODTProjectWithVelocity;
+import com.ecarvajal.xdoc.Project;
+
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
 
 @Controller
 public class VenderController {
@@ -89,10 +101,40 @@ public class VenderController {
 			System.out.println(" --- Se limpia la caja...");
 			venta.clear();
 			total=0;
+			generarODT();
+	          File outFile = new File("ODTProjectWithVelocity_Outh.odt");
+	          System.out.println("-- Ruta del fichero: |"+outFile.getAbsolutePath()+"|");
+	          System.out.println(" --- Se limpia la caja...");
 		}
 		
 		return "redirect:/"+"vender";
 	}
+	
+	public void generarODT() {
+	    try {
+	        System.out.println("Generamos reporte");
+	          // 1) Load ODT file by filling Velocity template engine and cache it to the registry
+	          InputStream in = ODTProjectWithVelocity.class.getResourceAsStream("ticket.odt");
+	          IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,TemplateEngineKind.Velocity);
+	          System.out.println(" --- Tramitada la venta.");
+	          // 2) Create context Java model
+	          IContext context = report.createContext();
+	          Project project = new Project("XDocReport");
+	          context.put("project", project);
+
+	          // 3) Generate report by merging Java model with the ODT
+	          File outFile = new File("ODTProjectWithVelocity_Outh.odt");
+	          System.out.println("-- Ruta del fichero: |"+outFile.getAbsolutePath()+"|");
+	          OutputStream out = new FileOutputStream(outFile);
+	          report.process(context, out);
+	          System.out.println("Creado ticket");
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        } catch (XDocReportException e) {
+	          e.printStackTrace();
+	        }
+	      }
+	
 	
 
 
--- tareas/src/main/java/com/ecarvajal/xdoc/ODTProjectWithVelocity.java
+++ tareas/src/main/java/com/ecarvajal/xdoc/ODTProjectWithVelocity.java
@@ -21,19 +21,23 @@ public class ODTProjectWithVelocity {
 	 */
   public  void xdox() {
     try {
+    	File outFile = new File("ODTProjectWithVelocity_Outh.odt");
+    System.out.println("Generamos reporte");
       // 1) Load ODT file by filling Velocity template engine and cache it to the registry
-      InputStream in = ODTProjectWithVelocity.class.getResourceAsStream("ODT.odt");
-      IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,TemplateEngineKind.Freemarker);
-
+      InputStream in = ODTProjectWithVelocity.class.getResourceAsStream("ticket.odt");
+      IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,TemplateEngineKind.Velocity);
+      System.out.println(" --- Tramitada la venta.");
       // 2) Create context Java model
       IContext context = report.createContext();
       Project project = new Project("XDocReport");
       context.put("project", project);
 
       // 3) Generate report by merging Java model with the ODT
-      OutputStream out = new FileOutputStream(new File("ODTProjectWithVelocity_Out.odt"));
+      
+      System.out.println("-- Ruta del fichero: |"+outFile.getAbsolutePath()+"|");
+      OutputStream out = new FileOutputStream(outFile);
       report.process(context, out);
-
+      System.out.println("Creado ticket");
     } catch (IOException e) {
       e.printStackTrace();
     } catch (XDocReportException e) {
