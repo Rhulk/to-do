4a7918fa9465286df2b318669dc7eab69f15ff40
kk <quique1904@gmail.com>
2020-10-29 18:27:02 +0100

    Imple logica del detallecliente falta la vista.

--- tareas/src/main/java/com/ecarvajal/controller/ClientesController.java
+++ tareas/src/main/java/com/ecarvajal/controller/ClientesController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import com.ecarvajal.model.Cliente;
 import com.ecarvajal.service.Listas;
 
 @Controller
@@ -16,16 +18,37 @@ public class ClientesController {
 
 	@Autowired
 	Listas list = new Listas();
+	public int id_detalle;
 	
-	List<Listas> clientes = new LinkedList<Listas>();
+	List<Cliente> clientes = new LinkedList<Cliente>();
 	
 	@GetMapping("/listclientes")
 	public String list(Model vista) {
 		System.out.println("Vista listado clientes");
-		 
+		clientes = list.getClientes();
 		vista.addAttribute("clientes", list.getClientes());// recuperamos los clientes seteados.
 		
 		return "clientes/list";
-	}	
+	}
+	
+	@GetMapping("/detallecliente/{id}")
+	public String detalle(@PathVariable("id") int id) {
+		System.out.println(" -- Detalle cliente vista id:"+ id);
+		id_detalle=id;
+		return "redirect:/"+"detallecliente";
+	}
+	
+	@GetMapping("/detallecliente")
+	public String detail(Model vista) {
+		int id=id_detalle;
+		// buscamos al cliente
+		for (int i=0; i < clientes.size() ; i++ ) {
+			if(clientes.get(i).getId() == id) {
+				vista.addAttribute("cliente", clientes.get(i) );
+			}
+		}
+		return "clientes/detallecliente";
+	}
+	
 
 }
