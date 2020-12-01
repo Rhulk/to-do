692114493e877879b11ea6506cf1507c8f0b201c
kk <quique1904@gmail.com>
2020-12-01 18:41:38 +0100

    Imp Cliente service add busquedas mas vinculo con el controller

--- tareas/src/main/java/com/ecarvajal/service/impl/IClienteService.java
+++ tareas/src/main/java/com/ecarvajal/service/impl/IClienteService.java
@@ -1,9 +1,17 @@
 package com.ecarvajal.service.impl;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import com.ecarvajal.model.Cliente;
 
 
 public interface IClienteService {
 	boolean guardar(Cliente cliente);
+	Optional<Cliente> burcarPorID(Integer id);
+	List<Cliente> buscarTodos();
+	boolean borrarPorID(Integer id);
+	
+	
 }
