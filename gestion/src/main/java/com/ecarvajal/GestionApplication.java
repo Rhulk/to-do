c51d576d175499817827f5f4c5ea73065c9f32d5
kk <quique1904@gmail.com>
2020-12-03 10:03:10 +0100

    up

---
+++ gestion/src/main/java/com/ecarvajal/GestionApplication.java
@@ -0,0 +1,13 @@
+package com.ecarvajal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApplication.class, args);
+	}
+
+}
---
+++ gestion/src/test/java/com/ecarvajal/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecarvajal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
